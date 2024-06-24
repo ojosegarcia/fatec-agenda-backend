a86b7e4e4bd0ded9a3baca1ba26a733287cf772e
ojosegarcia <josecarlosgarciajunior2003@gmail.com>
2024-06-24 15:38:52 -0300

    Put, Post e Delete Mapping

--- src/main/java/com/fatec/contato/resources/ContatoController.java
+++ src/main/java/com/fatec/contato/resources/ContatoController.java
@@ -5,12 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.contato.entities.Contato;
 import com.fatec.contato.services.ContatoService;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("contatos")
@@ -18,8 +24,24 @@ public class ContatoController {
 
     @Autowired
     private ContatoService contatoService;
+
     @GetMapping
     public List<Contato> getContatos() {
     return contatoService.getContatos();
+    }
+
+    @GetMapping("{id}")
+    public Contato getContatoById(@PathVariable int id){
+        return contatoService.getContatoByContato(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteContatoById(@PathVariable int id){
+        this.contatoService.deleteContatoById(id);
+    }
+
+    @PostMapping
+    public Contato save(@RequestBody Contato contato){
+        return this.contatoService.save(contato);
     }
 }
--- src/main/java/com/fatec/contato/services/ContatoService.java
+++ src/main/java/com/fatec/contato/services/ContatoService.java
@@ -2,19 +2,54 @@ package com.fatec.contato.services;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fatec.contato.entities.Contato;
 import com.fatec.contato.repositories.ContatoRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class ContatoService {
- @Autowired
- private ContatoRepository  contatoRepository;
+    @Autowired
+    private ContatoRepository contatoRepository;
 
- public List<Contato> getContatos(){
-    return  contatoRepository.findAll();
- }
+    public List<Contato> getContatos() {
+        return contatoRepository.findAll();
+
+    }
+
+    public Contato getContatoByContato(int id) {
+        return contatoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Contato não encontrado"));
+    }
+
+    public void deleteContatoById(int id) {
+        if (this.contatoRepository.existsById(id)) {
+            this.contatoRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Contato não encontrado");
+        }
+
+    }
+
+    public Contato save(Contato contato) {
+        return this.contatoRepository.save(contato);
+    }
+
+    public void update(int id, Contato contato) {
+        try {
+            Contato aux = contatoRepository.getReferenceById(id);
+            aux.setAddress(contato.getAddress());
+            aux.setDescription(contato.getDescription());
+            aux.setEmail(contato.getEmail());
+            aux.setName(contato.getName());
+            aux.setPhone(contato.getPhone());
+            this.contatoRepository.save(aux);
+
+        } catch (Exception e) {
+              throw new EntityNotFoundException("Contato não cadastrado");  
+        }
+    }
 }
