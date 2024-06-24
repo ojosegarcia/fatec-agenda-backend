4752a6c066557800192c44f97853c26745477fc0
ojosegarcia <josecarlosgarciajunior2003@gmail.com>
2024-06-24 08:23:03 -0300

    first commit

---
+++ src/main/java/com/fatec/contato/entities/Contato.java
@@ -0,0 +1,53 @@
+package com.fatec.contato.entities;
+
+
+
+public class Contato {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String description;
+    private String email;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+   
+
+
+}
